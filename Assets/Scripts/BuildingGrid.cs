3ac336bc3064e7402218bc95c0e4f7013839286f
Cole Porter <poby19@gmail.com>
2026-08-20 00:25:02 +1200

    Can see grid, need to fix tho

--- Assets/Scripts/BuildingGrid.cs
+++ Assets/Scripts/BuildingGrid.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingGrid : MonoBehaviour
@@ -6,16 +7,66 @@ public class BuildingGrid : MonoBehaviour
     [SerializeField] private int height;
     private BuildingGridCell[,] grid;
 
-    private void Start() {
+    private void Start()
+    {
         grid = new BuildingGridCell[width, height];
-        for (int x=0; x<grid.GetLength(0); x++)
+        for (int x = 0; x < grid.GetLength(0); x++)
         {
-            for (int y = 0; y < grid.GetLength(1); y++) {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
                 grid[x, y] = new();
             }
+        }
     }
 
-    
+    public void SetBuilding(Building building, List<Vector3> allBuildingPositions)
+    {
+        foreach (var p in allBuildingPositions )
+        {
+            (int x, int y) = WorldToGridPositon(p);
+            grid[x, y].SetBuilding(building);
+        }
+    }
+
+    public bool CanBuild(List<Vector3> allBuildingPositions)
+    {
+        foreach (var p in allBuildingPositions)
+        {
+            (int x, int y) = WorldToGridPositon(p);
+            if(x<0||x>=width||y<0||y>=height) return false;
+            if(!grid[x, y].IsEmpty())return false;
+        }
+        return true;
+    }
+
+    private (int x, int y) WorldToGridPositon(Vector3 worldPosition)
+    {
+        int x = Mathf.FloorToInt((worldPosition - transform.position).x / BuildingSystem.cellSize);
+        int y = Mathf.FloorToInt((worldPosition - transform.position).z / BuildingSystem.cellSize);
+        return (x, y);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        if(BuildingSystem.cellSize <=0||width<=0||height<=0) return;
+        Vector3 origin = transform.position;
+        for (int y = 0; y <= height; y++)
+        {
+            Vector3 start = origin + new Vector3(0, 0.01f,y*BuildingSystem.cellSize);
+            Vector3 end = origin + new Vector3(width * BuildingSystem.cellSize, 0.01f, y * BuildingSystem.cellSize);
+            Gizmos.DrawLine(start, end);
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            Vector3 start = origin + new Vector3(x*BuildingSystem.cellSize, 0.01f, 0);
+            Vector3 end = origin + new Vector3(width*BuildingSystem.cellSize, 0.01f, height*BuildingSystem.cellSize );
+            Gizmos.DrawLine(start, end);
+        }
+        
+    }
+
 
 }
 
